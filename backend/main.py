eb05217b0f9c261c049cd22b9b4174642e62152f
seiyu0225 <seiyu0225@gmail.com>
2021-09-19 20:26:02 +0900

    [delete] firebase info

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,6 @@ from flask import *
 from api import text_count_bp
 import pyrebase
 
-config = {
-  "apiKey" : "AIzaSyBqw-_4pLOOAMArM8k6Iqla37aWk1T3enY",
-  "authDomain" : "flaskonheroku.firebaseapp.com",
-  "projectId" : "flaskonheroku",
-  "storageBucket" : "flaskonheroku.appspot.com",
-  "messagingSenderId" : "644162721537",
-  "appId" : "1:644162721537:web:ed2cfbd05bb5919e706d00",
-  "measurementId" : "G-80ZC9Y9D9H",
-  "databaseURL" : "https://post_text.asia-northeast3.firebasedatabase.com"
-}
-
 app = Flask(__name__, static_folder='./templates/static', template_folder='./templates')
 # app = Flask(__name__)
 app.register_blueprint(text_count_bp)
@@ -25,21 +14,14 @@ def index(path):
 @app.route('/api/user_name/post', methods=['POST']) 
 # basic関数を定義
 def name_post():
-    # POSTでリクエストされたら
-    firebase = pyrebase.initialize_app(config)
-    db = firebase.database()
-    # formから値を取得
-    name = request.json['name']
-    post_text = db.child("post_text").order_by_child("name").equal_to(name).get()
-    
-    ret_object = []
-    for dic in post_text.each():
-        ret_object.append(
+    ret = []
+    for _ in range(5):
+        ret.append(
             {
-                "text" : dic.val()["text"]
+                "text" : "aaa"
             }
         )
-    return jsonify(ret_object)
+    return jsonify(ret)
 
 
 if __name__ == '__main__':
