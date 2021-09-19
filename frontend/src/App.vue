f8c3e9a2b7e335c32cac0a0f07c899906deec9ef
seiyu0225 <seiyu0225@gmail.com>
2021-09-19 18:41:59 +0900

    [change] backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,36 @@
 <template>
   <v-app>
-    <v-container fluid>
-      <v-row align="start" justify="center">
-        <v-col cols="10">
-          <v-textarea
-          outlined
-          name="input-7-4"
-          label="テキストを入力してください"
-          v-model="InputText"
-        ></v-textarea>
-        </v-col>
-        <v-col cols="2">
-          <v-btn outlined @click="SendData"> 文字数をカウント </v-btn>
-        </v-col>
-      </v-row>
-
-      <v-row align="start" justify="center">
-        <v-col cols="6">
-        <v-card
-          max-width="450"
-          class="mx-auto"
-        >
-          <v-toolbar
-            dark
-          >
-            <v-toolbar-title>Result</v-toolbar-title>
-          </v-toolbar>
-
-          <v-list three-line>
-            <template v-for="(item, index) in items">
-              <v-list-item
-                :key="item.title"
-              >
-                <v-list-item-content>
-                  <v-list-item-title >{{ item.count }}文字です</v-list-item-title>
-                  <v-list-item-subtitle> {{ item.text }} </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider
-                :key="index"
-                :inset="item.inset"
-              ></v-divider>
-            </template>
-          </v-list>
-        </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-text-field
+      v-model="name"
+      label="ペンネーム"
+      outlined
+    ></v-text-field>
+    <v-btn
+      elevation="2"
+      @click="SendData"
+    ></v-btn>
+   <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
+    >
+    <div v-for="PostedText in PostedTextList" v-bind:key="PostedText.created_at">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>PostedText.text</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+    <v-textarea
+      v-model="NewText"
+      color="teal"
+    >
+    <template v-slot:label>
+      <div>
+        新しいメモ
+      </div>
+    </template>
+    </v-textarea>
   </v-app>
 </template>
 
@@ -59,20 +43,20 @@ export default {
   data () {
     return {
       // 入力データ
-      InputText: '',
-      TextLength: null,
-      items: []
+      NewText: '',
+      name : '',
+      PostedTextList: []
     }
   },
 
   methods: {
-    SendData: function () {
-      var data = { text: this.InputText }
+    InputName: function () {
+      var myname = { text: this.name }
 
       axios
-        .post('/api/post', data)
+        .post('/api/user_name/post', myname)
         .then(response => {
-          this.items.push(response.data)
+          this.PostedText.push(response.data)
         })
         .catch(err => {
           alert('APIサーバと接続できません')
