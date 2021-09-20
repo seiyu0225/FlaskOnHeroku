af9e520118bb8b0914c820c10398c144ce3242aa
okai <seiyu0225@gmail.com>
2021-09-20 19:12:30 +0900

    [add] vueファイルの更新デザインを整える

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,46 @@
 <template>
   <v-app>
-    <v-container class="grey lighten-5 mb-6">
-      <v-text-field
-        v-model="name"
-        label="ペンネーム"
-        outlined
-      ></v-text-field>
-      <v-btn
-        elevation="2"
-        @click="InputName"
-        color="primary"
-      >送信</v-btn>
-      <v-card
-        class="mx-auto"
-        max-width="400"
-        tile
-      >
-      <div v-for="PostedText in PostedTextList" v-bind:key="PostedText.index">
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>{{PostedText.text}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </div>
-    <v-textarea
-        v-model="NewText"
-        color="teal"
-      >
-        <template v-slot:label>
-          <div>
-            新しいメモ
-          </div>
-        </template>
-      </v-textarea>
+    <v-container class=" lighten-5 mb-6">
+        <form>
+          <v-text-field
+            v-model="name"
+            label="Name"
+            required
+          ></v-text-field>
+          <v-btn
+            elevation="2"
+            @click="InputName"
+            color="primary"
+          >Get posted notes</v-btn>
+        </form>
+        <form>
+          <v-text-field
+            v-model="NewText"
+            :error-messages="ValidationErrors"
+            :counter="200"
+            label="New Notes"
+            required
+          ></v-text-field>
+          <v-btn
+            class="mr-4"
+            @click="Submit"
+            color="primary"
+          >
+            post
+          </v-btn>
+        </form>
+        <v-card
+          class="mx-auto"
+          max-width="400"
+        >
+          <v-card-title>Posted Notes</v-card-title>
+          <v-card-sub-title class="grey--text">Show notes that have already been posted</v-card-sub-title>
+          <v-card-text>
+            <div v-for="PostedText in PostedNotesList" v-bind:key="PostedText.index">
+              <div class="body-1 mb-1">{{PostedText.text}}</div>
+            </div>
+          </v-card-text>
+        </v-card>
     </v-container>
   </v-app>
 </template>
@@ -47,14 +55,14 @@ export default {
     return {
       // 入力データ
       NewText: '',
-      name : '',
-      PostedTextList: []
+      Name : '',
+      PostedNotesList: []
     }
   },
 
   methods: {
     InputName: function () {
-      var myname = { name: this.name }
+      var myname = { name: this.Name }
 
       axios
         .post('/api/user_name/post', myname) 
@@ -65,9 +73,22 @@ export default {
           this.PostedTextList = response.data;
         })
         .catch(err => {
-          alert('APIサーバと接続できません')
+          this.PostedNotesList.push(
+            {
+              index : 1,
+              text : this.NewText
+            }
+          )
           console.log("err :", err);
         })
+    },
+    Submit: function () {
+      this.PostedNotesList.push(
+        {
+          index : 1,
+          text : this.NewText
+        }
+      )
     }
   }
 }
